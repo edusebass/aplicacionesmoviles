1e0dbe0252b53eba206fc7135439ad0bf803a3d0
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-05-15 22:59:51 -0500

    splash screen terminada

--- photo-gallery/capacitor.config.ts
+++ photo-gallery/capacitor.config.ts
@@ -3,7 +3,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 2000,
+      backgroundColor: "#fff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#fff",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+    },
+  },
 };
 
 export default config;
