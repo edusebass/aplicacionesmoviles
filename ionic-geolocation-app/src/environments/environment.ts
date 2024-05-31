4deb53eaf3bbe0a90108a7579086e49200a3e954
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-05-31 16:01:55 -0500

    add enviroments

--- ionic-geolocation-app/src/environments/environment.ts
+++ ionic-geolocation-app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCI8Cq8qujSQqE4tJyrKJxr7fvuOMuut0E",
+    authDomain: "moviles-fb202.firebaseapp.com",
+    projectId: "moviles-fb202",
+    storageBucket: "moviles-fb202.appspot.com",
+    messagingSenderId: "366184884728",
+    appId: "1:366184884728:web:c051286ff8dc9b9007ce0d",
+    measurementId: "G-K52W6YYVDC"
+  }
 };
 
 /*
